fc9bfa4ba4971907bce03443c18a8d05147d643b
bodog <bodog@DESKTOP-39D644H.edu.ds.fhnw.ch>
2017-10-31 11:12:05 +0100

    test

--- IT-Projekt_Dominion/src/Cards/Class1.java
+++ IT-Projekt_Dominion/src/Cards/Class1.java
@@ -3,8 +3,8 @@ package Cards;
 public class Class1 {
 
 	public static void main(String[] args) {
-
 		System.out.println("Is everyone able to see this?");
+		System.out.println("Bodo war hier");
 	}
 
 }
