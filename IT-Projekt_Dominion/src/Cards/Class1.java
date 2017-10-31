cf40d9b86b5bb91359b7ac31c67f92b6f04f5921
Lukas <Lukas@DESKTOP-1BU7COR.edu.ds.fhnw.ch>
2017-10-31 09:17:25 +0100

    hhh

--- IT-Projekt_Dominion/src/Cards/Class1.java
+++ IT-Projekt_Dominion/src/Cards/Class1.java
@@ -5,16 +5,12 @@ public class Class1 {
 	public static void main(String[] args) {
 
 		System.out.println("Is everyone able to see this?");
-<<<<<<< HEAD
 		System.out.println("Lukas changed this!!");
 		
 		
 		while(true){
 			break;
 		}
-=======
-		System.out.println("Adrian changed this!!");
->>>>>>> branch 'master' of https://github.com/Eagleman1997/IT-Projekt_Dominion.git
 	}
 
 }
