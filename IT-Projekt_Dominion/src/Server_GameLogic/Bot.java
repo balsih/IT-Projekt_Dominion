243e2f2b92b23ebc41ca18cc53e318759ad85a85
Simon <Simon@192.168.0.17>
2017-11-09 18:04:04 +0100

    Bot update

--- IT-Projekt_Dominion/src/Server_GameLogic/Bot.java
+++ IT-Projekt_Dominion/src/Server_GameLogic/Bot.java
@@ -83,7 +83,7 @@ public class Bot extends Player {
 			if (actioncardlist.contains("Village_Card")) {
 				index = this.handCards.indexOf(Village_Card);
 				playedCard = this.handCards.remove(index);
-				playedCard.executeCard(this); jj
+				playedCard.executeCard(this); jjj
 				playedCards.add(playedCard);
 
 				actions--;
