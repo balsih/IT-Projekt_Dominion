bee7b5073c0f153d163e1ebfde76361f0779671b
Lukas <Lukas@192.168.192.50>
2017-11-12 15:36:17 +0100

    Signed-off-by: Lukas <Lukas@192.168.192.50>

--- IT-Projekt_Dominion/src/Messages/Commit_Message.java
+++ IT-Projekt_Dominion/src/Messages/Commit_Message.java
@@ -1,6 +1,8 @@
 package Messages;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * @author Lukas
@@ -25,7 +27,12 @@ public class Commit_Message extends Message {
 	 */
 	@Override
 	protected void addNodes(Document docIn){
-
+        Element root = docIn.getDocumentElement();
+		
+		Element notification = docIn.createElement(ELEMENT_NOTIFICATION);
+		notification.setTextContent(this.notification);
+		notification.setAttribute(ATTR_MOVE, this.move);
+		root.appendChild(notification);
 	}
 
 	/**
@@ -34,7 +41,35 @@ public class Commit_Message extends Message {
 	 */
 	@Override
 	protected void init(Document docIn){
+		Element root = docIn.getDocumentElement();
+		
+		NodeList tmpElements = root.getElementsByTagName(ELEMENT_NOTIFICATION);
+        if (tmpElements.getLength() > 0) {
+            Element notification = (Element) tmpElements.item(0);
+            this.notification = notification.getTextContent();
+            this.move = notification.getAttribute(ATTR_MOVE);
+        }
+	}
 
+	
+	
+	public String getNotification() {
+		return notification;
+	}
+	
+
+	public String getMove() {
+		return move;
+	}
+	
+
+	public void setNotification(String notification) {
+		this.notification = notification;
+	}
+
+
+	public void setMove(String move) {
+		this.move = move;
 	}
 	
 }//end CommitTransmission_Message
