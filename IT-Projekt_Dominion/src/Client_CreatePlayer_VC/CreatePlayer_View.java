68ed97b61767695e4b1c5b2f81da89a095f9770f
René Schwab <Ren Schwab@10.0.1.40>
2017-11-12 17:14:18 +0100

    cnp

--- IT-Projekt_Dominion/src/Client_CreatePlayer_VC/CreatePlayer_View.java
+++ IT-Projekt_Dominion/src/Client_CreatePlayer_VC/CreatePlayer_View.java
@@ -7,15 +7,20 @@ import Client_GameApp_MVC.GameApp_Model;
 import Client_Services.ServiceLocator;
 import Client_Services.Translator;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -39,7 +44,7 @@ public class CreatePlayer_View extends View<GameApp_Model> {
 	 */
 	public CreatePlayer_View(Stage stage, GameApp_Model model){
 		super(stage, model);
-		stage.initStyle(StageStyle.TRANSPARENT);
+		//stage.initStyle(StageStyle.TRANSPARENT);
 	}
 
 	@Override
@@ -49,33 +54,44 @@ public class CreatePlayer_View extends View<GameApp_Model> {
 		//sl.setTranslator(new Translator("de"));
 		Translator t = sl.getTranslator();
 		
+		// layouts
 		GridPane root = new GridPane();
-		
-		
-		//VBox root = new VBox();
-		
 		root.setId("root");
-		Button save = new Button(t.getString("cnp.saveButton"));
-		save.setId("save");
-		TextField name_text = new TextField();
-		name_text.setId("name_text");
-		TextField password_text = new TextField();
-		password_text.setId("password_text");
+		VBox vBox = new VBox();
+		vBox.setId("vBox");
+		
+		// labels
 		Label createNewPlayer = new Label(t.getString("cnp.createNewPlayer"));
 		createNewPlayer.setId("createNewPlayer");
+		//createNewPlayer.getStyleClass().add("createNewPlayer");  --> alternative Zuweisung zu CSS 
 		Label name = new Label(t.getString("cnp.name"));
 		name.setId("name");
 		Label password = new Label(t.getString("cnp.password"));
 		password.setId("password");
 		
-//		ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList("Deutsch", "Englisch"));
-//		cb.setTooltip(new Tooltip("Wähle eine Sprache"));
-//		cb.setPrefSize(20, 30);
-
+		// buttons
+		Button save = new Button(t.getString("cnp.save"));
+		save.setId("save");
+		Button back = new Button(t.getString("cnp.back"));
+		save.setId("back");
+		HBox buttonBox = new HBox(save, back);
+		buttonBox.setId("buttonBox");
+		
+		// textfields
+		TextField name_text = new TextField();
+		name_text.setId("name_text");
+		TextField password_text = new TextField();
+		password_text.setId("password_text");
+		
+		// language selection with ComboBox
+		ObservableList<String> language = FXCollections.observableArrayList(t.getString("program.german"), t.getString("program.english"));
+		final ComboBox cb = new ComboBox(language);
+		cb.setTooltip(new Tooltip(t.getString("program.languageTip")));
+		cb.setPrefSize(300.0, 15.0);
+		
 		
 		// https://panjutorials.de/tutorials/javafx-8-gui/lektionen/audio-player-in-javafx-2/?cache-flush=1510439948.4916 
-		// hier legen wir die Resource an, welche unbedingt im
-		// entsprechenden Ordner sein muss
+		// hier legen wir die Resource an, welche unbedingt im entsprechenden Ordner sein muss
 		final URL resource = getClass().getResource("sound.mp3");
 		// wir legen das Mediaobjekt and und weisen unsere Resource zu
 		final Media media = new Media(resource.toString());
@@ -85,27 +101,21 @@ public class CreatePlayer_View extends View<GameApp_Model> {
 		mediaPlayer.play();
 		//mediaPlayer.stop();
 		
-		root.add(createNewPlayer, 0, 0, 4, 2);
-		root.add(name, 0, 2, 4, 2);
-		root.add(name_text, 0, 4, 4, 2);
-		root.add(password, 0, 6, 4, 2);
-		root.add(password_text, 0, 8, 4, 2);
-		root.add(save, 0, 10, 4, 2);
 		
-		
-		root.setPrefSize(1280,720); 		
+		root.setPrefSize(1280,720);
+		//vBox.setPrefSize(300,200);
 		
 		//save.setAlignment(Pos.BOTTOM_RIGHT);
 		//root.setVgrow(name, Priority.ALWAYS);
 		
 		root.setAlignment(Pos.CENTER);
+		vBox.getChildren().addAll(createNewPlayer, name, name_text, password, password_text, cb, buttonBox);
+		root.getChildren().add(vBox);
 		
-		//root.getChildren().addAll(name, name_text, password, password_text, save);
-		
-
 		
 		Scene scene = new Scene(root);		
-		scene.getStylesheets().addAll(this.getClass().getResource("CreatePlayer.css").toExternalForm());
+		//scene.getStylesheets().addAll(this.getClass().getResource("CreatePlayer.css").toExternalForm());
+		scene.getStylesheets().add(getClass().getResource("CreatePlayer.css").toExternalForm());
 		this.stage.setScene(scene);
 		
 		return scene;
