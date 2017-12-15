8b75ccd944e36a39d7798dceb2d2d4250e221b4a
Lukas <Lukas@192.168.192.50>
2017-12-15 17:03:30 +0100

    fixed PlayerSuccess
    Signed-off-by: Lukas <Lukas@192.168.192.50>

--- IT-Projekt_Dominion/src/Messages/PlayerSuccess_Message.java
+++ IT-Projekt_Dominion/src/Messages/PlayerSuccess_Message.java
@@ -13,7 +13,8 @@ import Server_GameLogic.Player;
  */
 public class PlayerSuccess_Message extends Message {
 
-	private static final String ELEMENT_PLAYER_NAME = "playerName";
+	private static final String ELEMENT_PLAYER = "player";
+	private static final String ATTR_PLAYER_NAME = "playerName";
 	private static final String ELEMENT_SUCCESS = "success";
 	private static final String ELEMENT_VICTORY_POINTS = "victoryPoints";
 	private Integer numOfPlayers = 2;
@@ -42,8 +43,8 @@ public class PlayerSuccess_Message extends Message {
         		player = this.player2;
         	}
         	
-            Element playerElement = docIn.createElement(ELEMENT_PLAYER_NAME);
-            playerElement.setTextContent(player.getPlayerName());
+            Element playerElement = docIn.createElement(ELEMENT_PLAYER);
+            playerElement.setAttribute(ATTR_PLAYER_NAME, player.getPlayerName());
             root.appendChild(playerElement);
             
             Element success = docIn.createElement(ELEMENT_SUCCESS);
@@ -65,30 +66,34 @@ public class PlayerSuccess_Message extends Message {
 	protected void init(Document docIn){
 		Element root = docIn.getDocumentElement();
 		
-		NodeList tmpElements = root.getElementsByTagName(ELEMENT_PLAYER_NAME);
-		
+		NodeList tmpElements = root.getElementsByTagName(ELEMENT_PLAYER);
         for(int i = 0; i < tmpElements.getLength(); i ++) {
         	
-            Element playerName = (Element) tmpElements.item(i);
-            Player player = new Player(playerName.getTextContent(), null);
-            
-    		tmpElements = playerName.getElementsByTagName(ELEMENT_SUCCESS);
-            if (tmpElements.getLength() > 0) {
-                Element success = (Element) tmpElements.item(0);
-                player.setStatus(GameSuccess.parseGameSuccess(success.getTextContent()));
+            Element playerElement = (Element) tmpElements.item(i);
+            if(playerElement.hasAttribute(ATTR_PLAYER_NAME)){
+            	String playerName = playerElement.getAttribute(ATTR_PLAYER_NAME);
+            	
+                Player player = new Player(playerName, null);
+                
+        		NodeList subElements = playerElement.getElementsByTagName(ELEMENT_SUCCESS);
+                if (subElements.getLength() > 0) {
+                    Element success = (Element) subElements.item(0);
+                    player.setStatus(GameSuccess.parseGameSuccess(success.getTextContent()));
+                }
+                
+                subElements = playerElement.getElementsByTagName(ELEMENT_VICTORY_POINTS);
+                if(subElements.getLength() > 0){
+                	Element victoryPoints = (Element) subElements.item(0);
+                	player.setVictoryPoints(Integer.parseInt(victoryPoints.getTextContent()));;
+                }
+                
+                if(i == 0){
+            		this.player1 = player;
+            	}else{
+            		this.player2 = player;
+            	}
             }
             
-            tmpElements = playerName.getElementsByTagName(ELEMENT_VICTORY_POINTS);
-            if(tmpElements.getLength() > 0){
-            	Element victoryPoints = (Element) tmpElements.item(0);
-            	player.setVictoryPoints(Integer.parseInt(victoryPoints.getTextContent()));;
-            }
-            
-            if(i == 0){
-        		this.player1 = player;
-        	}else if(i == 1){
-        		this.player2 = player;
-        	}
         }
 	}
 	
