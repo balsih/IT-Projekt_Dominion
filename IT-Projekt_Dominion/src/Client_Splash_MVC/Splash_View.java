18bb9c930408bf5f6e7a74b3fdee98dfa52022b5
René Schwab <Ren Schwab@10.0.1.40>
2017-12-22 10:38:17 +0100

    comments

--- IT-Projekt_Dominion/src/Client_Splash_MVC/Splash_View.java
+++ IT-Projekt_Dominion/src/Client_Splash_MVC/Splash_View.java
@@ -1,30 +1,19 @@
 package Client_Splash_MVC;
 
-import java.net.URISyntaxException;
-
 import Abstract_MVC.View;
-import Client_Services.ServiceLocator;
-import Client_Services.Translator;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
- * @author Rene
- * @version 1.0
- * @created 31-Okt-2017 17:06:18
+ * View class for Splash screen 
+ * Defines controls and elements of this GUI, aligns and styles them.
+ * 
+ * @author Rene Schwab
  */
 public class Splash_View extends View<Splash_Model> {
 
@@ -32,24 +21,14 @@ public class Splash_View extends View<Splash_Model> {
 	private Label lblStatus;
 	
 
-	/**
-	 * 
-	 * @param stage
-	 * @param model
-	 */
 	public Splash_View(Stage stage, Splash_Model model){
 		super(stage, model);
-		//stage.initStyle(StageStyle.TRANSPARENT); // also undecorated
 	}
 
 	@Override
 	protected Scene create_GUI(){
 
-		ServiceLocator sl = ServiceLocator.getServiceLocator();
-		//sl.setTranslator(new Translator("de"));
-		Translator t = sl.getTranslator();
-		
-		lblStatus = new Label(t.getString("splash.label"));
+		lblStatus = new Label("Loading...");
 		lblStatus.setTextAlignment(TextAlignment.CENTER);
 		lblStatus.setId("lblStatus");
 		
@@ -60,8 +39,6 @@ public class Splash_View extends View<Splash_Model> {
 		root.setId("root");
 		
 		root.setAlignment(Pos.CENTER);
-		
-		
         
         Scene scene = new Scene(root, 300, 300);
         
